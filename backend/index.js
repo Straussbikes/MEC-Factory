1b638d86dd15471d39c9c98dcc26283397267628
Joao Neves <50676155+Straussbikes@users.noreply.github.com>
2023-09-20 19:58:06 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,24 +6,29 @@ import authRouter from './routes/auth.route.js';
 import cookieParser from "cookie-parser";
 
 const app = express();
-const whiteList=[process.env.ORIGIN1," https://frontend-dxox.onrender.com" ];
+const whiteList=[process.env.ORIGIN1," https://frontend-dxox.onrender.com",
+                44.226.145.213,
+                54.187.200.255,
+                34.213.214.55,
+                35.164.95.156,
+                44.230.95.183,
+                44.229.200.200
+                ];
 
 
-/*
 app.use(cors({
-    origin: function(origin,callback){
+    origin: function(origin, callback) {
         console.log(req.headers); 
-        if(!origin ||whiteList.includes(origin)){
+        if (!origin || whiteList.includes(origin)) {
             console.log(origin);
-            return callback(null,origin);
+            return callback(null, origin);
         }
         return callback(
-            "Error cors origin "  + origin
+            new Error("Error cors origin " + origin)
         );
     },
     credentials: true,
-}))
-*/
+}));
 app.use(cookieParser());
 app.use(express.json());
 app.use('',authRouter);
