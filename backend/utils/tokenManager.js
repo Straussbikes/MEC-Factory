ac37fb3ce8abc1927d678c2f01ffd5f08c2f7469
Joao Neves <50676155+Straussbikes@users.noreply.github.com>
2023-09-21 16:53:58 +0100

    Update tokenManager.js

--- backend/utils/tokenManager.js
+++ backend/utils/tokenManager.js
@@ -15,6 +15,8 @@ export const generateRefreshToken = (uid,res) =>{
     try {
         const refreshToken = jwt.sign({uid},process.env.JWT_REFRESH,{expiresIn});
         res.cookie("refreshToken",refreshToken,{
+            httpOnly: true,
+            secure:!(process.env.MODO == "developer"),
             expires: new Date(Date.now() + expiresIn*1000)
         });
     
